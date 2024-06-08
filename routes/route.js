a062bed9eb1c33879cd3727178b261f8c37d24a5
Sakpal Amit <amitsakpal744@gmail.com>
2024-06-08 13:36:29 +0530

    added route for our api

---
+++ routes/route.js
@@ -0,0 +1,12 @@
+// route api endpoint // 
+
+import express from 'express'
+import { SignupUser } from '../controller/user-controller.js';
+
+
+const router = express.Router();
+
+
+router.post('/signup' ,(SignupUser));
+
+export default router;
